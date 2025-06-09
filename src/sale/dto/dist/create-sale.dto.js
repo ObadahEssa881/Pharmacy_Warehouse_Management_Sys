4836a97d4bf877c7df527513927ae76a2164b572
obadah <obadahesa881@gmail.com>
2025-06-09 12:14:24 +0300

    adding purchases

---
+++ src/sale/dto/dist/create-sale.dto.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+exports.CreateSaleDto = void 0;
+var CreateSaleDto = /** @class */ (function () {
+    function CreateSaleDto() {
+    }
+    return CreateSaleDto;
+}());
+exports.CreateSaleDto = CreateSaleDto;
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,68 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { AuthModule } from 'src/auth/auth.module';
+import { PrismaModule } from '../prisma.module';
+import { PrismaService } from '../prisma.service';
+import { DMMFClass } from '@prisma/client/runtime';
+import { Database, Resource } from '@adminjs/prisma';
+import AdminJS from 'adminjs';
+import { authenticateUser } from './auth/user-auth.service'; // Import your user authentication function
+
+AdminJS.registerAdapter({ Database, Resource });
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    import('@adminjs/nestjs').then(({ AdminModule }) =>
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+      AdminModule.createAdminAsync({
+        imports: [PrismaModule],
+        useFactory: (prisma: PrismaService) => {
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+          const dmmf = (prisma as any)._baseDmmf as DMMFClass;
+
+          return {
+            adminJsOptions: {
+              rootPath: '/admin/users',
+              resources: [
+                {
+                  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+                  resource: { model: dmmf.modelMap['User'], client: prisma },
+                  options: {},
+                },
+              ],
+            },
+            auth: {
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+              authenticate: authenticateUser,
+              cookieName: 'adminjs-users',
+              cookiePassword: 'secret-users',
+            },
+            sessionOptions: {
+              resave: true,
+              saveUninitialized: true,
+              secret: 'secret-users',
+            },
+          };
+        },
+        inject: [PrismaService],
+      }),
+    ),
+  ],
 })
 export class UserModule {}
+
+// import { Module } from '@nestjs/common';
+// import { UserService } from './user.service';
+// import { UserController } from './user.controller';
+// import { AuthModule } from 'src/auth/auth.module';
+
+// @Module({
+//   controllers: [UserController],
+//   providers: [UserService],
+//   imports: [AuthModule],
+// })
+// export class UserModule {}
