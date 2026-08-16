44da5e59fe4615222a2bafefd9cc8a037bd8d09d
Obadah AboEssa <obadahesa881@gmail.com>
2026-08-16 14:30:08 +0300

    chore: stop tracking compiled dist/ output and fix .gitignore
    The ignore rule only matched /dist at the repo root, so 188 build artifacts
    under src/ and admin-panel/ were committed alongside the source.

--- admin-panel/src/auth/dist/roles.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports["default"] = {
-    OWNER: 'PHARMACY_OWNER',
-    ADMIN: 'SUPPLIER_ADMIN'
-};
--- src/common/decorators/dist/get‑user.decorator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.GetUser = void 0;
-var common_1 = require("@nestjs/common");
-exports.GetUser = common_1.createParamDecorator(function (_, ctx) {
-    var request = ctx.switchToHttp().getRequest();
-    return request.user;
-});
